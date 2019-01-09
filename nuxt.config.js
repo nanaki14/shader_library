573cf295d3765b2cfa4280d524214b84ce72dc8a
nanaki14 <masatochiba0122@gmail.com>
2019-01-09 23:32:19 +0900

    fix: 🐛 meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,60 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Shader Practice',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'nanaki14が書いたシェーダーを載せていくだけのサイトです。'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Shader Practice'
+      },
+      {
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://shader-practice.netlify.com/'
+      },
+      {
+        property: 'og:description',
+        content: 'nanaki14が書いたシェーダーを載せていくだけのサイトです。'
+      },
+      {
+        name: 'twitter:card',
+        content: 'summary'
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Shader Practice'
+      },
+      {
+        name: 'twitter:description',
+        content: 'nanaki14が書いたシェーダーを載せていくだけのサイトです。'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-16.png',
+        sizes: '16x16'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-32.png',
+        sizes: '32x32'
+      },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Charm'
